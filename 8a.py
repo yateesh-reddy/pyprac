a6b05ade2e817e16277d6fcd5e8506ef8f507172
Yateesh Reddy <reddyyateesh999@gmail.com>
2026-06-05 09:49:21 +0530

    Update 8a.py

--- 8a.py
+++ 8a.py
@@ -6,3 +6,9 @@ def area_rectangle(l, b):
 
 def area_circle(r):
     return 3.14*r*r
+
+import geometry
+
+print(geometry.area_square(5))
+print(geometry.area_rectangle(4, 6))
+print(geometry.area_circle(3))
